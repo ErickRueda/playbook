b7da557ed5d2bebe9e8a87d9fd44d4598c2275d0
Erick Rueda <itzdarklain@gmail.com>
2022-05-02 12:35:04 -0500

    agregando eslint

--- weekly_mission_4/fizzbuzz/lib/services/ExplorerService.js
+++ weekly_mission_4/fizzbuzz/lib/services/ExplorerService.js
@@ -1,21 +1,21 @@
 class ExplorerService {
 
     static filterByMission(explorers, mission){
-        const explorersByMission = explorers.filter((explorer) => explorer.mission === mission)
-        return explorersByMission
+        const explorersByMission = explorers.filter((explorer) => explorer.mission === mission);
+        return explorersByMission;
     }
 
     static getAmountOfExplorersByMission(explorers, mission){
-        const amountOfExplorersByMision = explorers.filter((explorer) => explorer.mission === mission).length
-        return amountOfExplorersByMision
+        const amountOfExplorersByMision = explorers.filter((explorer) => explorer.mission === mission).length;
+        return amountOfExplorersByMision;
 
     }
 
     static getExplorersUsernamesByMission(explorers, mission){
-        const explorersUsernamesByMission = explorers.filter((explorer) => explorer.mission === mission).map((explorer) => explorer.githubUsername)
-        return explorersUsernamesByMission
+        const explorersUsernamesByMission = explorers.filter((explorer) => explorer.mission === mission).map((explorer) => explorer.githubUsername);
+        return explorersUsernamesByMission;
     }
 
 }
 
-module.exports = ExplorerService
+module.exports = ExplorerService;
