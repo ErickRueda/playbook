ca44800a268d4f7511f8e100e32258b986ceb265
Erick Rueda <itzdarklain@gmail.com>
2022-05-02 12:22:32 -0500

    action

--- weekly_mission_4/fizzbuzz/index.js
+++ weekly_mission_4/fizzbuzz/index.js
@@ -15,6 +15,6 @@ console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
 // Aplicación del ExplorerService sobre la lista de explorers, busca en la lista de explorers los que tengan la misión node, y devuelve una lista con los nombres de los exploradores que tengan la misión node.
 console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
 
-// Aplicación del FizzbuzzService sobre la lista de explorers, asigna una propiedad trick a cada explorer, si el score es divisible por 3, el trick es FIZZ, si es divisible por 5, el trick es BUZZ, si es divisible por 3 y por 5, el trick es FIZZBUZZ, si no es divisible por ninguno, el trick es el score.
+// Aplicación del FizzbuzzService sobre la lista de explorers, asigna una propiedad trick a cada explorer, si el score es divisible por 3, el trick es FIZZ, si es divisible por 5, el trick es BUZZ, si es divisible por 3 y por 5, el trick es FIZZBUZZ, si no es divisible por ninguno el trick es el score.
 const fizzbuzzTrick = explorers.map((explorer) => FizzbuzzService.applyValidationInExplorer(explorer))
 
