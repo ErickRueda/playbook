a123f1ea47f458364512695f3928f6605a4e5949
Erick Rueda <99105167+ErickRueda@users.noreply.github.com>
2022-05-07 21:23:34 -0500

    update bot.js
    quitando token

--- weekly_mission_4/fizzbuzz/lib/bot.js
+++ weekly_mission_4/fizzbuzz/lib/bot.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const ExplorerController = require("./controllers/ExplorerController");
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = "5364314445:AAGTcWYvZYBWzcvszXoxoZY_ujzInshVCB8";
+const token = "";
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
